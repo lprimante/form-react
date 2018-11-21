925cad308e8b013f6ee4616173b3d3c134bc3995
Luciana Primante <lprimante@gmail.com>
2018-11-21 16:08:44 -0200

    Iniciando projeto RocketChat

---
+++ projeto/src/paginas/FormularioCadastro.js
@@ -0,0 +1,19 @@
+import React, { Component } from 'react'
+import './formularioCadastro.css'
+
+export default class formularioCadastro extends Component {
+	constructor(props) {
+		super(props)
+
+
+	}
+
+	render() {
+		return (
+			<main>
+				
+			</main>
+		)
+	}
+
+}
